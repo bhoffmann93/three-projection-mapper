cacc748d822488030aa4488139651d117119950a
Bernhard <b-hoffmann@live.de>
2026-02-09 16:28:00 +0100

    exports

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,4 @@
 export { ProjectionMapper, type ProjectionMapperConfig } from './ProjectionMapper';
 export { ProjectionMapperGUI, GUI_ANCHOR, GUI_STORAGE_KEY, type ProjectionMapperGUISettings } from './ProjectionMapperGUI';
-export { MeshWarper, type MeshWarperConfig } from './MeshWarper';
+export { MeshWarper, WARP_MODE, type MeshWarperConfig } from './MeshWarper';
 export { calculateGridPoints } from './geometry';
