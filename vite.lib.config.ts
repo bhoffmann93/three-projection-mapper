739fb7f16903ea10091c71b593b4b22dec3a3504
Bernhard <b-hoffmann@live.de>
2026-02-09 16:50:00 +0100

    Fixes type export

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
+import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl(),
+    dts({
+      tsconfigPath: './tsconfig.lib.json',
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
