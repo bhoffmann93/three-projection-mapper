89f64134e182403a0dd2650e9783451f70131c2b
bhoffmann93 <b-hoffmann@live.de>
2026-02-24 15:17:50 +0100

    Changes example resolution

--- examples/multi-window/multi-window.config.ts
+++ examples/multi-window/multi-window.config.ts
@@ -1,5 +1,5 @@
 export const MUTLI_WINDOW_CONFIG = {
-  projectionResolution: { width: 1920, height: 1080 },
+  projectionResolution: { width: 1280, height: 800 },
   bufferResOversampling: 1, //increase resolution for warping
 };
 
