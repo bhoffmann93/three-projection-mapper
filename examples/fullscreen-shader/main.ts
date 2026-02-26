4ca1ef8b3a39e03e14122997c6f90bede6bbfd13
bhoffmann93 <b-hoffmann@live.de>
2026-02-26 18:45:24 +0100

    Updates shader example

--- examples/fullscreen-shader/main.ts
+++ examples/fullscreen-shader/main.ts
@@ -56,20 +56,6 @@ const shaderMaterial = new THREE.ShaderMaterial({
         vec3 d = vec3(0.0, 0.10, 0.20);
         return a + b * cos(6.28318 * (c * t + d));
     }
-
-    //https://github.com/jamieowen/glsl-blend/blob/master/overlay.glsl
-
-    float blendOverlay(float base, float blend) {
-	    return base<0.5?(2.0*base*blend):(1.0-2.0*(1.0-base)*(1.0-blend));
-    }
-
-    vec3 blendOverlay(vec3 base, vec3 blend) {
-      return vec3(blendOverlay(base.r,blend.r),blendOverlay(base.g,blend.g),blendOverlay(base.b,blend.b));
-    }
-
-    vec3 blendOverlay(vec3 base, vec3 blend, float opacity) {
-	    return (blendOverlay(base, blend) * opacity + base * (1.0 - opacity));
-    } 
     
     void main() {
       vec2 uv = gl_FragCoord.xy / uResolution.xy;
@@ -120,7 +106,7 @@ const shaderMaterial = new THREE.ShaderMaterial({
       sumColor = acesApprox(sumColor);
 
       vec3 facade = texture2D(uTexture, uv).rgb;
-      vec4 finalColor = vec4(blendOverlay(facade, sumColor, 0.875), 1.0);
+      vec4 finalColor = vec4(0.5 * facade + sumColor  * 2.0 * facade.r, 1.0);
       gl_FragColor = finalColor;
     }
   `,
